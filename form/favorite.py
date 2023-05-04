1b1c075fe29054d3feda229e1ae75486036ed1c5
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2023-05-04 18:49:47 +0300

    Working favorite requests

--- form/favorite.py
+++ form/favorite.py
@@ -1,4 +1,6 @@
 from flask_restx import fields, Resource
+from sqlalchemy.exc import IntegrityError
+from flask import request
 
 from extensions import api, db
 from extensions.flask_restx_extension import favoriteNS
@@ -29,13 +31,18 @@ class FavoriteResourceList(Resource):
         400: 'Некорректный запрос'
     })
     @favoriteNS.expect(favorite_model)
-    @favoriteNS.marshal_with(favorite_model, code=201)
     def post(self):
-        favorite = Favorite(user_id=favoriteNS.payload.get('user_id'),
-                            restaurant_id=favoriteNS.payload.get('restaurant_id'))
-        db.session.add(favorite)
-        db.session.commit()
-        return favorite.to_dict(), 201
+        data = request.json
+        favorite = Favorite(user_id=data.get('user_id'),
+                            restaurant_id=data.get('restaurant_id'))
+        print(favorite.to_dict())
+        try:
+            db.session.add(favorite)
+            db.session.commit()
+            return favorite_schema.dump(favorite), 201
+        except IntegrityError as e:
+            db.session.rollback()
+            return {'message': 'Ошибка сохранения в базу данных. Неверные внешние ключи'}, 400
 
 
 class FavoriteResource(Resource):
@@ -47,7 +54,7 @@ class FavoriteResource(Resource):
     def get(self, id):
         favorite = Favorite.query.get(id)
         if not favorite:
-            favoriteNS.abort(404, 'Favorite not found')
+            favoriteNS.abort(404, 'Избранное не найдено')
         return favorite, 200
 
     @api.doc(responses={
@@ -58,20 +65,24 @@ class FavoriteResource(Resource):
     def put(self, id):
         favorite = Favorite.query.filter_by(id=id).first()
         if not favorite:
-            favoriteNS.abort(404, 'Favorite not found')
+            favoriteNS.abort(404, 'Избранное не найдено')
         favorite.user_id = favoriteNS.payload['user_id']
         favorite.restaurant_id = favoriteNS.payload['restaurant_id']
-        db.session.commit()
-        return favorite_schema.dump(favorite), 200
+        try:
+            db.session.commit()
+            return favorite_schema.dump(favorite), 200
+        except IntegrityError as e:
+            db.session.rollback()
+            return {'message': 'Ошибка сохранения в базу данных. Неверные внешние ключи'}, 400
 
     @api.doc(responses={
-        204: 'Успешный DELETE-запрос, ресурс удален',
+        200: 'Успешный DELETE-запрос, ресурс удален',
         404: 'Ресурс не найден'
     })
     def delete(self, id):
         favorite = Favorite.query.filter_by(id=id).first()
         if not favorite:
-            favoriteNS.abort(404, 'Favorite not found')
+            favoriteNS.abort(404, 'Избранное не найдено')
         db.session.delete(favorite)
         db.session.commit()
-        return {'result': 'success'}, 204
+        return {'msg': 'Удален из избранного'}, 200
