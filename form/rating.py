29820feae824a8ee21d0eb46f508e78318283245
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2023-06-06 17:06:22 +0300

    JWT for ratings

--- form/rating.py
+++ form/rating.py
@@ -1,4 +1,4 @@
-from extensions import ratingNS, api, db
+from extensions import ratingNS, api, db, jwt_required_class
 from models.models import Rating, User, Restaurant
 from schemas import RatingSchema
 from flask_restx import fields, Resource
@@ -13,12 +13,14 @@ rating_model = ratingNS.model('Rating', {
 })
 
 
+@jwt_required_class
 class RatingResourceList(Resource):
     @api.doc(responses={
         200: 'Успешный GET-запрос',
         400: 'Некорректный запрос'
     })
     @ratingNS.marshal_list_with(rating_model)
+    @ratingNS.doc(security='jwt')
     def get(self):
         ratings = Rating.query.all()
         return ratings, 200
@@ -29,6 +31,7 @@ class RatingResourceList(Resource):
     })
     @ratingNS.expect(rating_model)
     @ratingNS.marshal_with(rating_model, code=201)
+    @ratingNS.doc(security='jwt')
     def post(self):
         """Create a new rating"""
         data = api.payload
@@ -55,11 +58,13 @@ class RatingResourceList(Resource):
         return rating, 201
 
 
+@jwt_required_class
 class RatingResource(Resource):
     @api.doc(responses={
         200: 'Успешный GET-запрос',
         404: 'Ресурс не найден'
     })
+    @ratingNS.doc(security='jwt')
     @ratingNS.marshal_with(rating_model)
     def get(self, id):
         rating = Rating.query.filter_by(id=id).first()
@@ -71,6 +76,7 @@ class RatingResource(Resource):
         200: 'Успешный PUT-запрос',
         404: 'Ресурс не найден'
     })
+    @ratingNS.doc(security='jwt')
     @ratingNS.expect(rating_model)
     def put(self, id):
         rating = Rating.query.filter_by(id=id).first()
@@ -85,6 +91,7 @@ class RatingResource(Resource):
         204: 'Успешный DELETE-запрос, ресурс удален',
         404: 'Ресурс не найден'
     })
+    @ratingNS.doc(security='jwt')
     def delete(self, id):
         """Delete a rating by ID"""
         rating = Rating.query.get(id)
@@ -96,8 +103,10 @@ class RatingResource(Resource):
         return {'result': 'success'}, 204
 
 
+@jwt_required_class
 class AverageRatingResource(Resource):
     @ratingNS.doc(params={'restaurant_id': 'Restaurant ID'})
+    @ratingNS.doc(security='jwt')
     def get(self, restaurant_id):
         """Get the average rating for a restaurant by ID"""
         average_rating = db.session.query(db.func.avg(Rating.rating)).filter_by(restaurant_id=restaurant_id).scalar()
