dcd2dfd258c846c36283cdf11f6f8325800d3530
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2023-06-09 12:18:44 +0300

    Config file

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+from datetime import timedelta
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    # Database
+    DB_USERNAME = os.getenv('DB_USERNAME')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    DB_NAME = os.getenv('DB_NAME')
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
+    # JWT Token
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_HEADER_NAME = 'Authorization'
+    JWT_HEADER_TYPE = None
+    JWT_TOKEN_LOCATION = 'headers'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)  # Время истечения access token
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)  # Время истечения refresh token
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from datetime import timedelta
-
 from flask import Flask
 from flask_cors import CORS
 
-from extensions import db, api, ma, uploadNS, jwt, loginNS, ratingNS
-from extensions import favoriteNS, profileNS, reservationNS, restaurantNS, tableNS, cardNS, userNS, authNS, authWebNS
 from extensions import categoryNS
+from extensions import db, api, ma, uploadNS, jwt, ratingNS
+from extensions import favoriteNS, profileNS, reservationNS, restaurantNS, tableNS, cardNS, userNS, authNS, authWebNS
+from form import AuthResource
 from form import CardResource, CardResourceList, CategoryResourceList, CategoryResource, RatingResourceList, \
     RatingResource, TokenRefresh
 from form import FavoriteResource, FavoriteResourceList
@@ -13,11 +12,11 @@ from form import ProfileResource, ProfileResourceList, UploadProfilePic
 from form import ReservationResource, ReservationListResource
 from form import RestaurantListResource, RestaurantResource
 from form import TableResource, TableResourceList
-from form import UserResource, UserResourceList, UserEmailResource
-from form import AuthResource
-from form import WebAuthResource, ReservationStatusOne, ReservationStatusZero
 from form import UploadImage
+from form import UserResource, UserResourceList, UserEmailResource
+from form import WebAuthResource, ReservationStatusOne, ReservationStatusZero
 from form.rating import AverageRatingResource
+from config import Config
 
 
 def register_resource(api):
@@ -90,29 +89,7 @@ def register_resource(api):
 def create_app():
     # App configs
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret_key'
-    app.config['JSON_AS_ASCII'] = False
-    app.config['ERROR_INCLUDE_MESSAGE'] = False
-    app.config['PROPAGATE_EXCEPTIONS'] = True
-
-    # Upload files
-    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
-    # Database
-    # DB_USERNAME = 'loserMaru'
-    # DB_PASSWORD = '4863826M'
-    # DB_HOST = 'loserMaru.mysql.pythonanywhere-services.com'
-    # DB_NAME = 'loserMaru$mytable'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:4863826M@localhost/mystolik'
-
-    # JWT Token
-    app.config['JWT_SECRET_KEY'] = 'super-secret-key'
-    app.config['JWT_HEADER_NAME'] = 'Authorization'
-    app.config['JWT_HEADER_TYPE'] = None
-    app.config['JWT_TOKEN_LOCATION'] = 'headers'
-    app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)  # Время истечения access token
-    app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=30)  # Время истечения refresh token
+    app.config.from_object(Config)
 
     # Inits
     db.init_app(app)
