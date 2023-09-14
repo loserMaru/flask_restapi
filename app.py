645632a8b8570fd9e7b377fb63b3d609fe90c3cb
Marlen Osmanov <osmanov.m.i.20@gmail.com>
2023-09-14 15:27:24 +0300

    main.py renamed to app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_migrate import Migrate
 
 from extensions import categoryNS
 from extensions import db, api, ma, uploadNS, jwt, ratingNS
