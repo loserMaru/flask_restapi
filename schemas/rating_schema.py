c4f1f2adc61cde042a780370b698655285541fa4
Maru <osmanov.m.i.20@gmail.com>
2023-12-06 12:40:37 +0300

    rating float data type

--- schemas/rating_schema.py
+++ schemas/rating_schema.py
@@ -3,6 +3,6 @@ from marshmallow import Schema, fields
 
 class RatingSchema(Schema):
     id = fields.Integer(dump_only=True)
-    rating = fields.String(required=True)
+    rating = fields.Float(required=True)
     user_id = fields.Integer(required=True)
     restaurant_id = fields.Integer(required=True)
